2de72f682961bc801b33d4d7faa295a10c7447d0
Callum Taylor <callum@3sidedcube.com>
2014-09-26 15:10:37 +0100

    Added basic javadoc for point, circle, and root geojson object

--- library/src/main/java/com/cube/geojson/Circle.java
+++ library/src/main/java/com/cube/geojson/Circle.java
@@ -1,31 +1,88 @@
 package com.cube.geojson;
 
+/**
+ * (Unofficial) circle support with the format
+ *
+ * <pre>
+ *     {
+ *         "type": "Circle",
+ *         "coordinates": [
+ *              125.6, 10.1
+ *         ],
+ *         "radius": 1000.0
+ *     }
+ * </pre>
+ */
 public class Circle extends Point
 {
-	private double radius;
+	/**
+	 * The radius in metres
+	 */
+	protected double radius;
 
+	/**
+	 * Class constructor
+	 *
+	 * @param coordinates The coordinate for the point
+	 * @param radius The radius (in metres) of the circle
+	 */
 	public Circle(LngLatAlt coordinates, double radius)
 	{
 		super(coordinates);
 		this.radius = radius;
 	}
 
+	/**
+	 * Class constructor
+	 *
+	 * @param point The base point coordinates to use
+	 * @param radius The radius (in metres) of the circle
+	 */
+	public Circle(Point point, double radius)
+	{
+		super(point.getCoordinates());
+		this.radius = radius;
+	}
+
+	/**
+	 * Class constructor
+	 *
+	 * @param longitude The longitude coordinate for the point
+	 * @param latitude The longitude coordinate for the point
+	 * @param radius The radius (in metres) of the circle
+	 */
 	public Circle(double longitude, double latitude, double radius)
 	{
 		super(longitude, latitude);
 		this.radius = radius;
 	}
 
+	/**
+	 * Class constructor
+	 *
+	 * @param longitude The longitude coordinate for the point
+	 * @param latitude The longitude coordinate for the point
+	 * @param altitude The altitude (in metres) for the point
+	 * @param radius The radius (in metres) of the circle
+	 */
 	public Circle(double longitude, double latitude, double altitude, double radius)
 	{
 		super(longitude, latitude, altitude);
 		this.radius = radius;
 	}
 
+	/**
+	 * Gets the radius of the circle (in metres)
+	 *
+	 * @return The radius of the circle (in metres)
+	 */
 	public double getRadius() {
 		return radius;
 	}
 
+	/**
+	 * Sets the radius of the circle (in metres)
+	 */
 	public void setRadius(double radius) {
 		this.radius = radius;
 	}
--- library/src/main/java/com/cube/geojson/Point.java
+++ library/src/main/java/com/cube/geojson/Point.java
@@ -1,31 +1,89 @@
 package com.cube.geojson;
 
+/**
+ * A simple geometry point with a Longitude and Latitude coordinate
+ *
+ * <pre>
+ *     {
+ *         "type": "Point",
+ *         "coordinates": [
+ *              125.6, 10.1
+ *         ]
+ *     }
+ * </pre>
+ */
 public class Point extends GeoJsonObject
 {
+	/**
+	 * The coordinates of the point
+	 */
 	protected LngLatAlt coordinates;
 
+	/**
+	 * Class constructor
+	 *
+	 * @param point The base point coordinates to copy from (PBV)
+	 */
+	public Point(Point point)
+	{
+		this.coordinates = point.getCoordinates();
+	}
+
+	/**
+	 * Class constructor
+	 *
+	 * @param coordinates The lat, lng, alt coordianes to use
+	 */
 	public Point(LngLatAlt coordinates)
 	{
 		this.coordinates = coordinates;
 	}
 
+	/**
+	 * Class constructor
+	 *
+	 * @param longitude The longitude coordinate for the point
+	 * @param latitude The latitude coordinate for the point
+	 */
 	public Point(double longitude, double latitude)
 	{
 		coordinates = new LngLatAlt(longitude, latitude);
 	}
 
+	/**
+	 * Class constructor
+	 *
+	 * @param longitude The longitude coordinate for the point
+	 * @param latitude The latitude coordinate for the point
+	 * @param altitude The altitude (in metres) for the point
+	 */
 	public Point(double longitude, double latitude, double altitude)
 	{
 		coordinates = new LngLatAlt(longitude, latitude, altitude);
 	}
 
+	/**
+	 * Gets the coordinates of the point
+	 *
+	 * @return The coordinates of the point
+	 */
 	public LngLatAlt getCoordinates()
 	{
 		return coordinates;
 	}
 
+	/**
+	 * Sets the coordinates of the point
+	 *
+	 * @param coordinates The new coordinates of the point
+	 */
 	public void setCoordinates(LngLatAlt coordinates)
 	{
 		this.coordinates = coordinates;
+	}
+
+	@Override public String toString()
+	{
+		return "[com.cube.geojson.Point type: " + type + " coordinates: " + coordinates + "]";
 	}
 }
